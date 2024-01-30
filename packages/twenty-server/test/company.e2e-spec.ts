ac971abf56d77cd896067b01126caee90ef69cfe
martmull <martmull@hotmail.fr>
2024-01-30 09:37:12 +0100

    Fix app create test

--- packages/twenty-server/test/company.e2e-spec.ts
+++ packages/twenty-server/test/company.e2e-spec.ts
@@ -13,10 +13,12 @@ describe('CompanyResolver (e2e)', () => {
   const authGuardMock = { canActivate: (): any => true };
 
   beforeEach(async () => {
-    [app] = await createApp({
-      moduleBuilderHook: (moduleBuilder) =>
-        moduleBuilder.overrideGuard(JwtAuthGuard).useValue(authGuardMock),
-    });
+    app = (
+      await createApp({
+        moduleBuilderHook: (moduleBuilder) =>
+          moduleBuilder.overrideGuard(JwtAuthGuard).useValue(authGuardMock),
+      })
+    )[0];
   });
 
   afterEach(async () => {
